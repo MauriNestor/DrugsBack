9deed2db1a4522dbab432634665d83e1761df319
MauriNestor <mauric845apaza@gmail.com>
2025-01-27 23:03:12 -0400

    add in model  join between lab and product

--- src/main/java/com/scesi/farmacia/app/model/Laboratory.java
+++ src/main/java/com/scesi/farmacia/app/model/Laboratory.java
@@ -1,12 +1,15 @@
 package com.scesi.farmacia.app.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.Data;
+import java.util.List;
 
 @Data
 @Entity
@@ -26,4 +29,7 @@ public class Laboratory {
 
     @Column(name = "id_usuario")
     private Long userId;
+
+    @OneToMany(mappedBy = "laboratory", cascade = CascadeType.ALL)
+    private List<Product> products;
 }
